7cc9b69bdba452fc779ae6784cbeae2e905d08c6
JimLee12 <qkq010177@gmail.com>
2022-09-29 16:45:41 +0900

    22.09.29

--- HTML,CSS,JS_integrated/04.JavaPractice/tools/dom.js
+++ HTML,CSS,JS_integrated/04.JavaPractice/tools/dom.js
@@ -1,15 +1,30 @@
-  var i = 0;
+  let radios = 0;
   
+    document.querySelector(".No1")
   
   function getDice(event) {
-    i = document.getElementById('DiceResult').event.target.value;
-  }
-  
+    var radios = document.getElementsByName('DiceVaule');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+    // do whatever you want with the checked radio
+
+      // only one radio can be logically checked, don't check the rest
+      return radios[i].value;
+
+      }
+    }
+  };
+  // }
+  const reset = () => {
+    window.location.reload()
+  };
   
   const show = () => {
+ 
     // let OnOff = document.getElementsByClassName('DiceV');
     // OnOff.display = 'inline-block';
-
+  
     
     let tag = ""
     //1. 태그 만들기
@@ -20,28 +35,34 @@
     //    console.log(i)
     // }
     
-    const i = Math.floor(Math.random() * 6 + 1);
+    let i = Math.floor(Math.random() * 6 + 1);
     tag = tag + `<div class=divmsg><img src="tools/image/${i}.png" ></img id="msgImg"></div>`
     // `<div class="divMsg">${i}</div>`
        console.log(i)
-    
-
 
     //2. DOM에서 태그 넣을 요소 선택
     const m = document.querySelector('#msg')
-    
+    if(getDice() == i){
+      tag = tag + `<div class=divmsg><img src="tools/image/O.png" ></img id="msgImg"></div>`
+    }else{
+      tag = tag + `<div class=divmsg><img src="tools/image/X.png" ></img id="msgImg"></div>`
+    }
+    console.log()
     //3. 선택된 요소의 HTML 변경
     m.innerHTML = tag
+    
     
 
     // m.style.backgroundColor="red";
     //스타일 변경 console.log(m.getComputedStyle());
   
     //4. 동적 요소도 스타일 변경 가능.
-    let msgImg = document.querySelector('#msgImg')
-    msgImg.style.maxWidth="100px";
-    msgImg.style.maxheight="100px";
+    // let msgImg = document.querySelector('#msgImg')
+    // msgImg.style.maxWidth="100px";
+    // msgImg.style.maxheight="100px";
 
+    document.querySelector(".bt1").style.display = "none";
+    document.querySelector(".bt2").style.display = "inline-block";
   }
 
 /////////////////////////////라디오 버튼////////////
